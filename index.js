41e192224107dbc0610f95b8c7c14892132e7717
Штирбу Эдуард <60253601+leeroykahikki@users.noreply.github.com>
2023-12-08 18:04:37 +0300

    Добавлена возможность пинга сервера

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const { setupBot } = require('./bot');
 
 const PORT = process.env.PORT || 5000;
@@ -9,7 +8,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')));
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
 
 // рекурсивная асинхронная функция, которая запускает бота
 (async function () {
